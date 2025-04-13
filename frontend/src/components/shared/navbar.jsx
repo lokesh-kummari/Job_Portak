7ba0d93da17a40a764890548a15be7f56b98172b
lokesh <lokesh@MacBook-Air-2.local>
2025-04-13 19:22:37 +0530

    frontend updated

---
+++ frontend/src/components/shared/navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job<span className="text-[#F83002]">Portal</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
